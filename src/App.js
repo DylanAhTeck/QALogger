31cf8e82a90a05d56ec60b758a1d3851b6c73ecc
Dylan  Ah Teck <dylanahteck@Dylans-MacBook-Pro.local>
2019-07-08 23:17:45 -0700

    add searchbar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-import React, { useEffect } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import './App.css';
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
+import SearchBar from './components/layout/SearchBar';
 
 const App = () => {
   useEffect(() => {
     //Init Materialize JS
     M.AutoInit();
   });
-  return <div className='App'>My App</div>;
+
+  return (
+    <Fragment>
+      <SearchBar />
+    </Fragment>
+  );
 };
 
 export default App;
